f88904940c5d0ec11c8b7a237731961a772c6533
Arsalan Bashir <a@livedocs.com>
2021-12-16 20:21:17 +0400

    whatever

--- cron.js
+++ cron.js
@@ -1,1 +1,12 @@
-console.log("blah blah");
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('https://7865-2001-8f8-1263-a3c4-c54e-8df3-930f-4e10.ngrok.io')
+  .then(function (response) {
+    // handle success
+    console.log("SEND");
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
